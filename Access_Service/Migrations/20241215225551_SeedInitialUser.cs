9df153e4c36096114e49a5082d046d5f355587c5
GioxBRUSH <giovanny.buzeta@alumnos.ucn.cl>
2024-12-15 20:08:12 -0300

    Feat: Add initial user seeding migration with hashed password and UserUuid

---
+++ Access_Service/Migrations/20241215225551_SeedInitialUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Access_Service.Migrations
+{
+    /// <inheritdoc />
+  public partial class SeedInitialUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var email = "jane.doe@example.com";
+            var password = "hashed_password_123";
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            var userUuid = 1;
+
+            migrationBuilder.Sql($@"
+                INSERT INTO Users (Email, Password, UserUuid)
+                VALUES ('{email}', '{hashedPassword}', '{userUuid}')
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var email = "jane.doe@example.com";
+
+            migrationBuilder.Sql($@"
+                DELETE FROM Users
+                WHERE Email = '{email}'
+            ");
+        }
+    }
+}
